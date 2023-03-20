12db0beb0768e1ded584200ca5f413d03587a178
Kristian Trenskow <trenskow@me.com>
2023-03-20 09:44:46 +0100

    Added the ability to select the merge strategy.

--- index.js
+++ index.js
@@ -1,48 +1,99 @@
 //
 // index.js
 // @trenskow/app
-// 
+//
 // Created by Kristian Trenskow on 2021/11/15
 // For license see LICENSE.
-// 
+//
 
 const isObject = (value) => typeof value === 'object' && value !== null;
 
-const merge = (...values) => {
+const _merge = (options = {
+	object: 'merge',
+	array: 'merge',
+	other: 'second'
+}) => {
 
-	while (values.length > 1) {
-
-		let [ first, second ] = values;
-
-		if (!isObject(first) || !isObject(second)) {
-			first = second;
-		} else {
-			if (Array.isArray(first) && Array.isArray(second)) {
-				first = first.concat(second);
-			} else {
-
-				const firstKeys = Object.keys(first);
-				const secondKeys = Object.keys(second);
-
-				const oldKeys = firstKeys.filter((key) => !secondKeys.includes(key));
-				const newKeys = secondKeys.filter((key) => !firstKeys.includes(key));
-				const sharedKeys = firstKeys.filter((key) => secondKeys.includes(key));
-
-				first = Object.assign(
-					Object.fromEntries(oldKeys.map((key) => [key, first[key]])),
-					Object.fromEntries(sharedKeys.map((key) => [key, merge(first[key], second[key])])),
-					Object.fromEntries(newKeys.map((key) => [key, second[key]]))
-				);
-
-			}
-		}
-
-		values.splice(0, 2, first);
-
+	if (typeof options === 'string') {
+		options = {
+			object: options,
+			array: options,
+			other: options
+		};
 	}
 
-	return values[0];
+	return (...values) => {
+
+		while (values.length > 1) {
+
+			let [ first, second ] = values;
+
+			if (!isObject(first) || !isObject(second)) {
+				switch (options.other) {
+					case 'second':
+						first = second;
+						break;
+					default:
+						break;
+				}
+			} else {
+				if (Array.isArray(first) && Array.isArray(second)) {
+					switch (options.array) {
+						case 'merge':
+							first = first.concat(second);
+							break;
+						case 'second':
+							first = second;
+							break;
+						default:
+							break;
+					}
+				} else {
+
+					switch (options.object) {
+						case 'merge': {
+
+							const firstKeys = Object.keys(first);
+							const secondKeys = Object.keys(second);
+
+							const oldKeys = firstKeys.filter((key) => !secondKeys.includes(key));
+							const newKeys = secondKeys.filter((key) => !firstKeys.includes(key));
+							const sharedKeys = firstKeys.filter((key) => secondKeys.includes(key));
+
+							first = Object.assign(
+								Object.fromEntries(oldKeys.map((key) => [key, first[key]])),
+								Object.fromEntries(sharedKeys.map((key) => [key, merge(first[key], second[key])])),
+								Object.fromEntries(newKeys.map((key) => [key, second[key]]))
+							);
+
+							break;
+
+						}
+
+						case 'second':
+							first = second;
+							break;
+
+						default:
+							break;
+
+					}
+
+				}
+			}
+
+			values.splice(0, 2, first);
+
+		}
+
+		return values[0];
+
+	};
 
 };
+
+const merge = _merge();
+
+merge.strategy = _merge;
 
 export default merge;
